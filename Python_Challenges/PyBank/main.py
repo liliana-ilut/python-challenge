7884c68a15790a557de66a66ac9257e4a2691b7c
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-06-26 20:14:53 -0500

    final PyBank version

--- Python_Challenges/PyBank/main.py
+++ Python_Challenges/PyBank/main.py
@@ -1,44 +1,66 @@
 import os
-import csv
+import csv 
 
-#set the path for the csv file
-mainpath_csv = os.path.join ("budget_data.csv")
+    #set the path for the csv file
+csv_path = os.path.join("Resources", "budget_data.csv")
 
-#output file for the analysis text
-output_file = "Financial_Analysis.txt"
+    #print the path to make sure the path opens correctly. Mark the print in a cooment so it won't interact with our main code
+#print ("opening", csv_path)
 
-#open and read csv file
-with open(mainpath_csv, "r") as csvfile :
-    reader = csv.reader(csvfile, delimiter = ',')
+    #output file for the analysis text
+output_file = "financial_analysis.txt"
 
-    #read the header row first
-    csv_header = next(csvfile)
+    #open and read the csv file
+with open(csv_path, "r") as in_file:
+    csv_reader = csv.reader(in_file)
 
-    #print the header to check if code works before moving on and mark it as a comment so it won't interact with the actual code
+    #read first row's header and print the header to make sure it work, after that mark it as a comment so it won't interact with our code
+    csv_header = next(csv_reader)
     #print(f"Header: {csv_header}")
 
-months = []
-profit = []
-change = []
+    #creates lists to store data
+    pro_los = []
+    months = []
+    revenue_change = []
 
-# read through each row of data after header
-for row in reader :
-        profit.append(int(row[1]))
+    # seperate profit/losses and months using append 
+    for row in csv_reader:
+        pro_los.append(int(row[1]))
         months.append(row[0])
+    
+    # calculate total months and total profit/losses
+    total_months = len(months)
+    total_pro_los = sum(pro_los)
+    
+    # calculate change in revenue 
+    for i in range(1, len(pro_los)):
+        revenue_change.append((int(pro_los[i]) - int(pro_los[i-1])))
+    
+    # calculate average revenue change, greatest increase in profit and greatest decrease in losses
+    average_change = sum(revenue_change) / len(revenue_change)
+    great_increase_profit = max(revenue_change)
+    great_decrease_loss = min(revenue_change)
+  
+    # print out results 
+    print("financial analysis")
+    print("__________________________")
+    print("Total Months : " + str(total_months))
+    print("Total Profit/Losses : " + "$" + str(total_pro_los))
+    print("Average Change : " + "$" + str(round(average_change, 2)))
+    print("Greatest Increase in Profit : " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(great_increase_profit))
+    print("Greatest Decrease in profit : " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(great_decrease_loss))
 
-#calculate total months and total profit/Losses
-
-total_months = len(months)
-total_profit = sum(profit)
-
-# calculate the change in revenue 
-for i in range(1, len(profit)):
-    revenue_change.append((int(profit[i]) - int(profit[i-1])))
-
-print("Financial Analysis")
-print("__________________________")
-print("Total Months: " + str(total_months))
-print("Total: " + "$" + str(total_profit))
-print("Average Change: " + "$" + str(round(revenue_average, 2)))
-print("Greatest Profit Increase: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
-print("Greatest Profit Decrease: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
+    #export the financil analysis in a text file
+with open(output_file, "w+") as analysis_text:
+    analysis_text.write("financial analysis\n")
+    analysis_text.write("__________________________\n")
+    analysis_text.write("\n")
+    analysis_text.write("Total Months: " + str(total_months))
+    analysis_text.write("\n")
+    analysis_text.write("Total: " + "$" + str(total_pro_los))
+    analysis_text.write("\n")
+    analysis_text.write("Average Change: " + "$" + str(round(average_change, 2)))
+    analysis_text.write("\n")
+    analysis_text.write("Greatest Profit Increase: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(great_increase_profit))
+    analysis_text.write("\n")
+    analysis_text.write("Greatest Profit Decrease: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(great_decrease_loss))
